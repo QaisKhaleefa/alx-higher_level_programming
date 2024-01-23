1b30825bdd69182334bc74a01d9782716cf6f13e
QaisKhaleefa <qaisomer29@gmail.com>
2024-01-23 02:48:20 -0800

    last digit and upercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for n in str:
+        if ord(n) >= ord('a') and ord(n) <= ord('z'):
+            n = chr(ord(c) - ord(' '))
+
+        print("{}".format(n), end="")
+    print("")
